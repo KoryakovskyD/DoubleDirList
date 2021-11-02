6441e4f7a180f30ff4ab4de07273526224178641
KoryakovskyD <d.koryakovsky@gmail.com>
2021-11-03 00:50:34 +0300

    2 commit

--- src/ru/avalon/javapp/devj110/doubledirlist/DoubleDirList.java
+++ src/ru/avalon/javapp/devj110/doubledirlist/DoubleDirList.java
@@ -7,13 +7,13 @@ public class DoubleDirList {
 
     // добавление значения в начало списка
     public void addToHead(Object value) {
+        ListItem newItem = new ListItem(value);
         if (! isEmpty()) {
-            ListItem newItem = new ListItem(value);
+            head.prev = newItem;
             newItem.next = head;
-            newItem.prev = null;
             head = newItem;
         } else
-            head = tail = new ListItem(value);
+            head = tail = newItem;
     }
 
     // извлечение значения из начала списка без его удаления из списка
@@ -35,16 +35,97 @@ public class DoubleDirList {
 
     // добавление значения в конец списка
     public void addToTail(Object value) {
-        if (tail != null) {
-            tail.next = new ListItem(value);
-            tail = tail.next;
+        ListItem newItem = new ListItem(value);
+        if (! isEmpty()) {
+            tail.next = newItem;
+            newItem.prev = tail;
+            tail = newItem;
         } else
-            head = tail = new ListItem(value);
+            head = tail = newItem;
+    }
+
+    // извлечение значения из конца списка без его удаления
+    public Object peekFromTail() {
+        return tail != null ? tail.value : null;
+    }
+
+    // извлечение значения из конца списка с удалением
+    public Object removeFromTail() {
+        if (isEmpty())
+            return null;
+        Object res = tail.value;
+        if (head != tail) {
+            ListItem it = tail;
+            it.prev = tail;
+            tail.next = null;
+        } else
+            head = tail = null;
+        return res;
+    }
+
+    // определение, содержит ли список заданное значение, или нет
+    public boolean contains(Object value) {
+        ListItem it = head;
+        while (it != null) {
+            if(it.checkValue(value)) {
+                System.out.println("prev=" + it.prev.value);
+                return true;
+            }
+            it.prev = it;
+            it = it.next;
+        }
+        return false;
+    }
+
+    // определение, является ли список пустым, или нет
+    public boolean isEmpty() {
+        return head == null;
     }
 
 
-    public boolean isEmpty() {
-        return head == null;
+
+    // *удаление заданного значения из списка; если значения в списке нет, то ничего происходить не должно
+    public void remove(Object value) {
+        if (isEmpty())
+            return;
+        if (head.checkValue(value)) {
+            removeFromHead();
+            return;
+        }
+        ListItem it = head.next;
+        it.prev = head;
+        while (it != null) {
+            if (it.checkValue(value)) {
+                if (it.equals(tail))
+                    removeFromTail();
+                else {
+                    head.prev.next = it.next;
+                    it.prev = head.prev;
+                }
+                return;
+            }
+            head.prev = it;
+            it = it.next;
+        }
+
+    }
+
+    // *выполнение действия, заданного в параметре метода, для каждого значения из списка.
+
+    // добавление всех значений заданного массива в начало списка; порядок значений должен сохраняться
+    // — первое значение массива должно стать первым значением списка
+    public void addArrayInHead(Object[] values) {
+        for (int i = values.length; i> 0; i--) {
+            addToHead(values[i-1]);
+        }
+    }
+
+    // добавление всех значений заданного массива в конец списка; порядок значений должен сохраняться
+    // — первое значение массива должно стать первым значением списка
+    public void addArrayInTail(Object[] values) {
+        for (int i=0; i< values.length; i++) {
+            addToTail(values[i]);
+        }
     }
 
     // печать всех значений списка
@@ -56,20 +137,14 @@ public class DoubleDirList {
         }
     }
 
-
-
-    private static class ListItem {
-        Object value;
-        ListItem next;
-        ListItem prev;
-
-        public ListItem(Object value) {
-            this.value = value;
-        }
-
-        boolean checkValue(Object value) {
-            return value == null & this.value == null || value != null && value.equals(this.value);
+    // печать всех значений списка в обратном порядке
+    public void printAllRevers() {
+        ListItem it = tail;
+        while (it != null) {
+            System.out.println(it.value);
+            it = it.prev;
         }
     }
+
 
 }
--- src/ru/avalon/javapp/devj110/doubledirlist/Main.java
+++ src/ru/avalon/javapp/devj110/doubledirlist/Main.java
@@ -3,28 +3,64 @@ package ru.avalon.javapp.devj110.doubledirlist;
 public class Main {
     public static void main(String[] args) {
         DoubleDirList lst = new DoubleDirList();
-        lst.addToHead("111");
+
+        // дабавление элементов в начало и конец
+        lst.addToHead("222");
         lst.addToTail("333");
-        lst.addToTail("222");
+        lst.addToHead("111");
         lst.addToTail("444");
-        lst.printAll();
-        System.out.println();
-
-        /*
+        lst.addToTail("555");
+        lst.addToTail("666");
+        lst.addToTail("777");
         lst.addToHead("AAA");
+        lst.addToHead("DDD");
         lst.printAll();
         System.out.println();
 
-        lst.remove("222");
-        lst.remove("444");
-        lst.printAll();
-        System.out.println();
 
+
+        // содержит ли список значение
+        if (lst.contains("666"))
+            System.out.println("Содержит");
+        if(!lst.contains("BBB"))
+            System.out.println("Не содержит");
+        System.out.println();
+        //lst.printAllRevers();
+
+        // удаление элементов в начале и конце
         lst.removeFromHead();
         lst.removeFromTail();
         lst.printAll();
         System.out.println();
 
-         */
+
+
+
+        // удаление заданного элемента
+        lst.remove("222");
+        lst.remove("444");
+        lst.printAll();
+        System.out.println();
+
+        // добавление массива в начало
+        lst.addArrayInHead(new String[] {"first", "second","third"});
+        lst.printAll();
+        System.out.println();
+
+        // добавление массива в конец
+        lst.addArrayInTail(new String[] {"first_end", "second_end","third_end"});
+        lst.printAll();
+        System.out.println();
+
+        // печать массива в обратном порядке
+        //lst.printAllRevers();
+
+        if (lst.contains("666"))
+            System.out.println("Содержит");
+
+        if (lst.contains("second_end"))
+            System.out.println("Содержит");
+
+
     }
 }
